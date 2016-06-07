bd866166ff0c9fbc56af3a29e68acbe04e7133a7
tmk13 <tmk13@protonmail.ch>
2016-06-07 22:18:54 +0200

    done iss87

--- bookstore/src/main/java/com/apress/bookstore/service/BookService.java
+++ bookstore/src/main/java/com/apress/bookstore/service/BookService.java
@@ -45,6 +45,7 @@ public class BookService {
 		// second master branch change
 		// test new change
 		// test change
+		// iss87 change
 
 		//List<Book> allBooks = bookDao.findBookByCategory(category);
 		return allBooks;
